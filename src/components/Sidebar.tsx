848eb3ff734ded76e5a786eac35ea5ab423e141b
Apolo Moraes <vlogsapolo@gmail.com>
2023-02-17 14:43:49 -0300

    created Sidebar and Tweet components

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,41 @@
+import { House, Hash, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle, Sparkle } from 'phosphor-react'
+import twitterLogo from '../assets/logo-twitter.svg'
+import './global.css'
+
+
+export function Sidebar() {
+  return (
+    <aside className='sidebar'>
+      <img className='logo' src={twitterLogo} alt="Logo pássaro azul" />
+
+      <nav className='main-navigation' >
+        <a className='active' href="">
+          < House weight='fill' />
+          Home</a>
+        <a href="">
+          <Hash />
+          Explore</a>
+        <a href="">
+          <Bell />
+          Notifications</a>
+        <a href="">
+          <Envelope />
+          Messages</a>
+        <a href="">
+          <BookmarkSimple />
+          Bookmarks</a>
+        <a href="">
+          <FileText />
+          Lists</a>
+        <a href="">
+          <User />
+          Profile</a>
+        <a href="">
+          <DotsThreeCircle />
+          More</a>
+      </nav>
+
+      <button className='new-tweet' type='button' >Tweet</button>
+    </aside>
+  )
+}
--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,19 +1,42 @@
-interface TweetProps {
-  user: string
-  children: string
-  like?: number
-}
+import { ArrowsClockwise, ChatCircle, Heart } from 'phosphor-react'
+import './Tweet.css'
 
-const tweetStyles = {
-  borderRadius: 8
-}
-
-export function Tweet(props: TweetProps) {
+export function Tweet() {
   return (
-    <div className="tweet" style={tweetStyles}>
-      <strong>{props.user}</strong>
-      <p>{props.children}</p>
-      <button>Like {props.like ?? 0}</button>
-    </div>
+    <a href='#' className="tweet">
+      <img src="https://github.com/apolomoraes.png" alt="Foto do usuário" />
+
+      <div className="tweet-content">
+        <div className="tweet-content-header">
+          <strong>Apolo Moraes</strong>
+          <span>@apolomoraess</span>
+        </div>
+        <p>
+          Acabei de migrar um projeto React GIGANTE de create-react-app para Vite e os resultados foram:<br />
+          <br />
+          ✅ npm start: De 32s para 400ms (sim, demorava 30s)<br />
+          ✅ npm build: De 120s para 22s<br />
+          <br />
+          Além disso, troquei do Yarn para o PNPM e o install das deps mudou de 24s para 8s 🔥
+        </p>
+
+        <div className="tweet-content-footer">
+          <button type='button'>
+            <ChatCircle />
+            20
+          </button>
+
+          <button type='button'>
+            <ArrowsClockwise />
+            20
+          </button>
+
+          <button type='button'>
+            <Heart />
+            20
+          </button>
+        </div>
+      </div>
+    </a>
   )
 }
