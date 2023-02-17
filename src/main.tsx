b3236c0e1b8c3850c310044888a4b07b70335758
Apolo Moraes <vlogsapolo@gmail.com>
2023-02-17 16:22:28 -0300

    retirei a tag main do main.tsx e coloquei no Timeline.tsx da pasta pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Sparkle } from 'phosphor-react'
 
 import './global.css'
-import { Tweet } from './components/Tweet'
 import { Sidebar } from './components/Sidebar'
-import { Header } from './components/Header'
-import { Separator } from './components/Separator'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './components/routes'
-
-const tweets = [
-  'meu primeiro',
-  'meu segundo',
-  'meu terceiro'
-]
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -22,28 +12,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Sidebar />
 
       <div className='content'>
-        <main className='timeline' >
-          <Header title="Home" />
-
-          <RouterProvider router={router} />
-
-          <form className="new-tweet-form">
-            <label htmlFor="tweet">
-              <img src="https://github.com/apolomoraes.png" alt="Foto de usuário" />
-              <textarea id='tweet' placeholder="What's happening?" />
-            </label>
-
-            <button type='submit' >Tweet</button>
-          </form>
-
-          <Separator />
-
-          {
-            tweets.map(tweet => {
-              return <Tweet key={tweet} content={tweet} />
-            })
-          }
-        </main>
+        <RouterProvider router={router} />
       </div>
     </div>
   </React.StrictMode >,
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,4 +1,35 @@
+import { Header } from "../components/Header"
+import { Separator } from "../components/Separator"
+import { Tweet } from "../components/Tweet"
+
+const tweets = [
+  'meu primeiro',
+  'meu segundo',
+  'meu terceiro'
+]
 
 export function Timeline() {
+  return (
+    <main className='timeline' >
+      <Header title="Home" />
 
+
+      <form className="new-tweet-form">
+        <label htmlFor="tweet">
+          <img src="https://github.com/apolomoraes.png" alt="Foto de usuário" />
+          <textarea id='tweet' placeholder="What's happening?" />
+        </label>
+
+        <button type='submit' >Tweet</button>
+      </form>
+
+      <Separator />
+
+      {
+        tweets.map(tweet => {
+          return <Tweet key={tweet} content={tweet} />
+        })
+      }
+    </main>
+  )
 }
