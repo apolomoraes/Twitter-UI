e2e23dc6c55df538a0f466d285e04d3d5939f58a
Apolo Moraes <vlogsapolo@gmail.com>
2023-02-20 10:33:16 -0300

    add useState no Tweet na Timeline.tsx

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,24 +1,34 @@
+import { FormEvent, useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
 import './Timeline.css'
 
-const tweets = [
-  'meu primeiro',
-  'meu segundo',
-  'meu terceiro'
-]
-
 export function Timeline() {
+  const [newTweet, setNewTweet] = useState('')
+  const [tweets, setTweets] = useState([
+    'meu primeiro',
+    'meu segundo',
+    'meu terceiro'
+  ])
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault()
+
+    setTweets([newTweet, ...tweets])
+  }
+
   return (
     <main className='timeline' >
       <Header title="Home" />
 
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/apolomoraes.png" alt="Foto de usuário" />
-          <textarea id='tweet' placeholder="What's happening?" />
+          <textarea id='tweet' placeholder="What's happening?" value={newTweet} onChange={(event) => {
+            setNewTweet(event.target.value)
+          }} />
         </label>
 
         <button type='submit' >Tweet</button>
